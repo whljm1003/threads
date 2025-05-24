382d9054b2e8bcbbfa9908169a2cd5ee29894b6c
leejungmin <whljm1003@gmail.com>
2025-05-25 01:11:08 +0900

    update: customize refreshing

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,16 +1,26 @@
-import { View, StyleSheet, useColorScheme } from "react-native";
+import { View, StyleSheet, useColorScheme, PanResponder } from "react-native";
 import { usePathname } from "expo-router";
 import Post, { type Post as PostType } from "@/components/Post";
-import { useCallback, useState } from "react";
 import { FlashList } from "@shopify/flash-list";
 import * as Haptics from "expo-haptics";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { AnimationContext } from "./_layout";
+
+const AnimatedFlashList = Animated.createAnimatedComponent(FlashList<PostType>);
 
 export default function Index() {
   const colorScheme = useColorScheme();
   const path = usePathname();
-  const [refreshing, setRefreshing] = useState(false);
   const [posts, setPosts] = useState<PostType[]>([]);
-  console.log("posts", posts.length);
+  const scrollPosition = useSharedValue(0);
+  const isReadyToRefresh = useSharedValue(false);
+  const { pullDownPosition } = useContext(AnimationContext);
 
   const onEndReached = useCallback(() => {
     console.log("onEndReached", posts.at(-1)?.id);
@@ -23,33 +33,94 @@ export default function Index() {
       });
   }, [posts, path]);
 
-  const onRefresh = () => {
-    setRefreshing(true);
+  const onRefresh = (done: () => void) => {
     setPosts([]);
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
     fetch("/posts")
       .then((res) => res.json())
       .then((data) => setPosts(data.posts))
-      .finally(() => setRefreshing(false));
+      .finally(() => done());
   };
 
+  const onPanRelease = () => {
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 60 : 0, {
+      duration: 180,
+    });
+    console.log("onPanRelease", isReadyToRefresh.value);
+    if (isReadyToRefresh.value) {
+      onRefresh(() => {
+        pullDownPosition.value = withTiming(0, {
+          duration: 180,
+        });
+      });
+    }
+  };
+
+  const panResponderRef = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        const max = 120;
+        pullDownPosition.value = Math.max(Math.min(gestureState.dy, max), 0);
+        console.log("pull", pullDownPosition.value);
+
+        if (
+          pullDownPosition.value >= max / 2 &&
+          isReadyToRefresh.value === false
+        ) {
+          isReadyToRefresh.value = true;
+        }
+
+        if (
+          pullDownPosition.value < max / 2 &&
+          isReadyToRefresh.value === true
+        ) {
+          isReadyToRefresh.value = false;
+        }
+      },
+      onPanResponderRelease: onPanRelease,
+      onPanResponderTerminate: onPanRelease,
+    })
+  );
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      console.log("onScroll", event.contentOffset.y);
+      scrollPosition.value = event.contentOffset.y;
+    },
+  });
+
+  const pullDownStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: pullDownPosition.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View
+    <Animated.View
       style={[
         styles.container,
         colorScheme === "dark" ? styles.containerDark : styles.containerLight,
+        pullDownStyles,
       ]}
+      {...panResponderRef.current.panHandlers}
     >
-      <FlashList
+      <AnimatedFlashList
+        refreshControl={<View />}
         data={posts}
-        onRefresh={onRefresh}
-        refreshing={refreshing}
         renderItem={({ item }) => <Post item={item} />}
         onEndReached={onEndReached}
+        nestedScrollEnabled={true}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
         onEndReachedThreshold={2}
         estimatedItemSize={350}
       />
-    </View>
+    </Animated.View>
   );
 }
 
